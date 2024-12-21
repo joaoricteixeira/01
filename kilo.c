fb5092581b21681dfa15def830912b8b5126ffe3
João Teixeira <76978819+joaoricteixeira@users.noreply.github.com>
2024-12-21 20:37:03 +0000

    Sections Added error handling and sections

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h> //Most C library functions that fail will set the global errno variable to indicate what the error was.
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -6,15 +7,25 @@
 
 struct termios orig_termios;
 
+//Error handling
+//Function that prints an error message and exits the program
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+
+
+//Raw mode
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
-//Raw mode enabled for data entry
   struct termios raw = orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
@@ -23,13 +34,14 @@ void enableRawMode() {
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main() {
   enableRawMode();
    while (1) {
     char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     read(STDIN_FILENO, &c, 1);
     if (iscntrl(c)) {
       printf("%d\r\n", c);
